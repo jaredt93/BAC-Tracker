799c695cc4578471e638c4516205096457194181
Jared Tamulynas <jaredt93@vt.edu>
2022-11-02 21:06:36 -0400

    initial commit

---
+++ BAC Tracker/BAC Tracker/BAC_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BAC_TrackerApp.swift
+//  BAC Tracker
+//
+//  Created by Jared WIlliam Tamulynas on 11/2/22.
+//
+
+import SwiftUI
+
+@main
+struct BAC_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
