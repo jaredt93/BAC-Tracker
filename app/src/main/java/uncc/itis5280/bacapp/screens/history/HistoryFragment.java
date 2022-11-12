ec24c1f396127b7b8a436a5455b3bbdeaab3ebf2
Jared Tamulynas <jaredt93@vt.edu>
2022-11-12 12:38:45 -0500

    add chart

--- app/src/main/java/uncc/itis5280/bacapp/screens/history/HistoryFragment.java
+++ app/src/main/java/uncc/itis5280/bacapp/screens/history/HistoryFragment.java
@@ -1,5 +1,6 @@
 package uncc.itis5280.bacapp.screens.history;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,15 +10,27 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+
 import uncc.itis5280.bacapp.databinding.FragmentHistoryBinding;
 
 public class HistoryFragment extends Fragment {
     private FragmentHistoryBinding binding;
+    LineChart chart;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentHistoryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        chart = (LineChart) binding.chart;
+        chart.setTouchEnabled(true);
+        chart.setPinchZoom(true);
 
         return root;
     }
@@ -25,6 +38,39 @@ public class HistoryFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        if (getDataSet() != null) {
+            LineData data = new LineData(getDataSet());
+            chart.setData(data);
+        }
+//        chart.setDescription("My Chart");
+//        chart.animateXY(2000, 2000);
+//        chart.invalidate();
+
+    }
+
+    private LineDataSet getDataSet() {
+        ArrayList<Entry> values = new ArrayList<>();
+        values.add(new Entry(1, 0));
+        values.add(new Entry(2, (float).08));
+        values.add(new Entry(3, (float).06));
+        values.add(new Entry(4, (float).18));
+        values.add(new Entry(5, (float).28));
+
+        LineDataSet set = new LineDataSet(values, "set");
+        set.setColor(Color.rgb(0, 155, 0));
+        return set;
+    }
+
+    private ArrayList getXAxisValues() {
+        ArrayList xAxis = new ArrayList();
+        xAxis.add("JAN");
+        xAxis.add("FEB");
+        xAxis.add("MAR");
+        xAxis.add("APR");
+        xAxis.add("MAY");
+        xAxis.add("JUN");
+        return xAxis;
     }
 
     @Override
